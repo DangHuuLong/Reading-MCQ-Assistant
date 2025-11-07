e93b1be179eb395e69d433b96951d564f8aae731
Long Đặng Hữu <danghuulong394@gmail.com>
2025-11-07 21:41:23 +0700

    fix: load MySQL JDBC driver manually to resolve 'No suitable driver' error

--- src/main/java/com/readingmcqassistant/utils/DBConnection.java
+++ src/main/java/com/readingmcqassistant/utils/DBConnection.java
@@ -6,8 +6,16 @@ import java.sql.SQLException;
 
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3307/reading_mcq_assistant";
-    private static final String USER = "root";      
-    private static final String PASSWORD = "";     
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
